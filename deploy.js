d0d6023ad660f7f608a9654ad72f9cc5689e9744
EC2 Default User <ec2-user@ip-172-31-26-69.ap-northeast-2.compute.internal>
2021-11-29 09:01:10 +0000

    Tested on Ropsten Network The network issue was a local network configuration issue related to the test network. Downgrade Node.js version because of (error:03000086:digital envelope routines::initialization error)

--- deploy.js
+++ deploy.js
@@ -1,30 +1,28 @@
-// deploy code will go here
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-const Web3 = require('web3')
-const { interface, bytecode } = require('./compile')
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
 
 const path = require('path')
 const dotenv = require('dotenv')
 dotenv.config({ path: path.join(__dirname, '.env') })
-
-const provider = new HDWalletProvider(
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
   process.env.MNEMONIC,
   process.env.INFURA_ENDPOINT
-)
-const web3 = new Web3(provider)
-const INITIAL_STRING = 'Hello World!'
-
+);
+const web3 = new Web3(provider);
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts()
-  console.log(accounts[0])
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: [INITIAL_STRING],
-    })
-    .send({ from: accounts[0], gas: '1000000' })
-  console.log(result.options.address)
-  provider.engine.stop()
-}
-deploy()
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,39 +1,35 @@
-// contract test code will go here
-const assert = require('assert')
-const ganache = require('ganache-cli')
-const Web3 = require('web3')
-const web3 = new Web3(ganache.provider())
-const { interface, bytecode } = require('../compile')
-
-let accounts
-let inbox
-const INITIAL_STRING = 'Hello World!'
-
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+ 
+const { abi, evm } = require('../compile');
+ 
+let accounts;
+let inbox;
+ 
 beforeEach(async () => {
-  accounts = await web3.eth.getAccounts()
-
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
-      arguments: [INITIAL_STRING],
+      data: evm.bytecode.object,
+      arguments: ['Hi there!'],
     })
-    .send({ from: accounts[0], gas: '1000000' })
-})
-
+    .send({ from: accounts[0], gas: '1000000' });
+});
+ 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    assert.ok(inbox.options.address)
-  })
-
+    assert.ok(inbox.options.address);
+  });
   it('has a default message', async () => {
-    const message = await inbox.methods.message().call()
-    assert.equal(message, INITIAL_STRING)
-  })
-
-  it('can chage the message', async () => {
-    //   Looks it needs gas
-    await inbox.methods.setMessage('bye').send({ from: accounts[0] })
-    const message = await inbox.methods.message().call()
-    assert.equal(message, 'bye')
-  })
-})
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
+});
