469a35ec770ef8560af69352c76e4b211daf144f
EC2 Default User <jinnuae40@gmail.com>
2021-12-02 06:21:26 +0000

    Learn how to send and keep ETH in the contract. Use modifier to make reusable code. (address object).transfer(this.balance) for send ETH belong to the contract.

--- compile-inbox.js
+++ compile-inbox.js
@@ -2,15 +2,13 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
  
-// const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const inboxPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
  
 const input = {
   language: 'Solidity',
   sources: {
-    // 'Inbox.sol': {
-    'Lottery.sol': {
+    'Inbox.sol': {
       content: source,
     },
   },
@@ -24,7 +22,5 @@ const input = {
 };
  
 module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
-  'Lottery.sol'
-].Lottery;
-//   'Inbox.sol'
-// ].Inbox;
+  'Inbox.sol'
+].Inbox;
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,15 +1,38 @@
-pragma solidity ^0.8.9;
+pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
     address[] public players;
     
-    constructor() {
+    function Lottery() public {
         manager = msg.sender;
     }
 
     function enter() public payable {
         require(msg.value > .01 ether);
         players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(sha3(block.difficulty, now, players));
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+
+    function getBalance() public view restricted returns (uint) {
+        return uint(this.balance);
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
     }
 }
